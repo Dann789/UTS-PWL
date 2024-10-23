5e887c3ff9484a7cc84c7961afecd96c9569c609
Muhammad Wildan Ramadhana <wildan.rama2004@gmail.com>
2024-10-23 17:47:11 +0700

    update progress uts

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,12 @@ Route::get('/', [LandingPageController::class, 'index']);
 
 Route::pattern('id', '[0-9]+');
 
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('register', [AuthController::class, 'postregister']);
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::get('register', [AuthController::class, 'register'])->name('register');
-Route::post('register', [AuthController::class, 'postregister']);
 
 Route::get('/profile', [ProfileController::class, 'index']);
 Route::post('upload_foto', [ProfileController::class, 'upload_foto'])->name('upload.foto');
